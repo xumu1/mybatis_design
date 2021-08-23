e4dda5645e734ba02159d49fa1483d781a7b52f0
xumu <sunhaolong.shl@alibaba-inc.com>
2021-08-23 20:18:51 +0800

    m

--- src/main/java/com/xumu/mybatis/cfg/Mapper.java
+++ src/main/java/com/xumu/mybatis/cfg/Mapper.java
@@ -4,13 +4,20 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+/**
+ * @author xumu
+ */
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 public class Mapper {
 
-    //SQL语句
+    /**
+     * SQL语句
+     */
     private String queryString;
-    //实体类全限定类名
+    /**
+     * 实体类全限定类名
+     */
     private String resultType;
 }
